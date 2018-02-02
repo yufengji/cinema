4d25887660d0a432dd2fe6559e4db1fc9023093e
jyfdreamlove <793117069@qq.com>
2018-02-02 16:35:49 +0800

    do city

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="header">
-    header
+    <div class="h-title">
+      <div class="toolbar-title-icon">
+        <i class="iconfont icon-zhankai1"></i>
+      </div>
+      <div class="toolbar-title">
+        卖座电影
+      </div>
+    </div>
+    <div class="nav-right">
+      <a href="javascript:;" class="city">
+        <span class="city-content">厦门</span>
+        <i class="iconfont icon-zhankai"></i>
+      </a>
+      <a href="javascript:;" class="user">
+        <i class="iconfont icon-yonghu"></i>
+      </a>
+    </div>
   </div>
 </template>
 <script>
@@ -19,4 +35,38 @@ export default {
     left: 0
     right: 0
     overflow: hidden
+    .h-title
+      float: left
+      width: auto
+      .toolbar-title-icon
+        width: 100px
+        height: 100px
+        float: left
+        text-align: center
+        color: #999
+      .toolbar-title
+        padding: 0 20px
+        width: 360px
+        float: left
+        color: #efefef
+        text-overflow: ellipsis
+        white-space: nowrap
+        overflow: hidden
+    .nav-right
+      float: right
+      a
+        color: #999
+      .city
+        font-size: 28px
+        float: left
+        padding:0 10px
+        i
+          font-size: 24px
+      .user
+        font-size: 32px
+        float: left
+        width: 100px
+        text-align: center
+
+
 </style>
