8bda6b6a7d4ee9e617d02afee97af33bf287149d
jyfdreamlove <793117069@qq.com>
2018-02-26 09:12:03 +0800

    修改api接口对应开发环境

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"//127.0.0.1:8080/api"'
+  API_ROOT: '"//localhost:8080/api"'
 })
